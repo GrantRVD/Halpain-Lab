07ca05b65305b1a4e9886d81c49ebbd1201f1012
Grant R. Vousden-Dishington <gvousden@ucsd.edu>
2013-05-05 11:00:35 -0700

    Cloning the python code to github

---
+++ MetaMorphImageProcessor.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun May 05 10:19:14 2013
+
+@author: Grant
+"""
+
+# Import librariries needed to process TIFF images
+import Image as I
+import matplotlib.pyplot as plot
+import matplotlib.image as mi
+import numpy as n
+
+# TODO: Get directory from user
+
+
+# Read in the image
+
